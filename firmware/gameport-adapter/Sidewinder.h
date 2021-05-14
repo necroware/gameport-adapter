3ff643e00a19040d7c12cd79b185d768d0558a96
Filipp Andjelo <filipp@andjelo.de>
2021-05-14 11:56:17 +0200

    Implemented common base for sidwinder joysticks

--- firmware/gameport-adapter/Sidewinder.h
+++ firmware/gameport-adapter/Sidewinder.h
@@ -1,73 +1,52 @@
 #pragma once
 
 #include "DigitalPin.h"
+#include "Log.h"
 
 class Sidewinder {
 public:
 
-   struct Bits {
-       uint64_t data{0u};
-       uint16_t count{0u};
+   enum class Model {
+       SW_UNKNOWN,
+       SW_GAMEPAD,
+       SW_3DPRO
    };
 
-   Bits readBits() const {
-       InterruptStopper interruptStopper;
-       const auto ready = m_clock.isHigh();
-       m_trigger.setHigh();
-       Bits bits;
-       if (ready || m_clock.wait(Edge::rising, PULSE_DURATION * 10)) {
-           bits = readData();
-       }
-       m_trigger.setLow();
-       log("Packet length: ", bits.count);
-       return bits;
-   }
+   struct State {
+       int16_t axis[10]{0};
+       int32_t buttons{0};
+   };
 
-   bool validate(Bits bits) const {
-       auto checksum = 0u;
-       while (bits.data) {
-           checksum ^= bits.data & 1;
-           bits.data >>= 1;
-       }
-       return checksum == 0;
-   }
-
-   bool reset() const {
+   Model reset() {
        log("Resetting...");
-       cooldown();
-       auto bits = readBits();
-       if (!bits.count) { // No data. 3d Pro analog mode?
+       cooldown();       
+       m_model = guessModel(readPacket());
+       if (m_model == Model::SW_UNKNOWN) {
+         // No data. 3d Pro analog mode?
          enableDigitalMode();
          cooldown();
-         bits = readBits(); // Retry reading packet
-         if (bits.count == 0) { // No data -> FAIL
-           log("Failed to init driver");
-           return false;
-         }
+         m_model = guessModel(readPacket());
        }
-
-       bits = readBits();
-       bits = readBits();
        cooldown();
-       return true;
+       log("Detected model %d", m_model);          
+       return m_model;
    }
 
-private:
+   State readState() {
+       const auto packet = readPacket();
+       State state;
+       if (!decode(packet, state)) {
+           reset();
+       }
+       return state;
+   }
 
-   static void cooldown() {
+   void cooldown() const {
+       m_trigger.setLow();
        delayMicroseconds(6000);
    }
 
-   template <typename T, typename ...Args>
-   static void log(const T& head, Args... tail) {
-       Serial.print(head);
-       log(tail...);
-   }
-
-   template <typename T>
-   static void log(const T& head) {
-       Serial.println(head);
-   }
+private:
 
    enum {
       PULSE_DURATION = 60,
@@ -78,15 +57,33 @@ private:
        ~InterruptStopper() { interrupts();}
    };
 
+   struct Packet {
+       byte bits[256];
+       uint16_t length{0u};
+   };
+
+   template <Model M>
+   struct Decoder {
+       static bool decode(const Packet& packet, State& state);
+   };
+
+   static Model guessModel(const Packet& packet) {
+       switch(packet.length) {
+           case 15: return Model::SW_GAMEPAD;
+           case 64: return Model::SW_3DPRO;
+           default: return Model::SW_UNKNOWN;
+       }
+   }
+
    DigitalInput<GamePort<2>::pin, true> m_clock;
    DigitalInput<GamePort<7>::pin, true> m_data;
    DigitalOutput<GamePort<3>::pin> m_trigger;
+   Model m_model{Model::SW_UNKNOWN};
 
    void enableDigitalMode() const {
       static const int magic = 200;
       static const int seq[] = { magic, magic + 725, magic + 300, 0 };
       log("Trying to enable digital mode");
-      m_trigger.setLow();
       cooldown();
       InterruptStopper interruptStopper;
       for (auto i = 0u; seq[i]; i++) {
@@ -96,15 +93,75 @@ private:
       m_trigger.pulse(PULSE_DURATION);
    }
 
-   Bits readData() const {
-       Bits bits;
-       for (auto i = 0u; i < 256; i++) {             
-           if (!m_clock.wait(Edge::rising, PULSE_DURATION * 2)) {
-               break;
+   Packet readPacket() const {
+       InterruptStopper interruptStopper;
+       const auto ready = m_clock.isHigh();
+       m_trigger.setHigh();
+       Packet packet;
+       if (ready || m_clock.wait(Edge::rising, PULSE_DURATION * 10)) {
+           while(packet.length < sizeof(packet.bits)) { 
+               if (!m_clock.wait(Edge::rising, PULSE_DURATION)) {
+                   break;
+               }
+               packet.bits[packet.length++] = m_data.get();
            }
-           bits.data = (bits.data << 1) | m_data.get();
-           bits.count++;
        }
-       return bits;
+       m_trigger.setLow();
+       delayMicroseconds(PULSE_DURATION);
+       return packet;
    }
+
+   bool decode(const Packet& packet, State& state) const;
 };
+
+template <>
+class Sidewinder::Decoder<Sidewinder::Model::SW_GAMEPAD> {
+public:
+    static bool decode(const Sidewinder::Packet& packet, Sidewinder::State& state) {
+
+        const auto checksum = [&](){
+            byte result = 0u;
+            for (auto i = 0u; i < packet.length; i++) {
+               result ^= packet.bits[i] & 1;
+            }
+            return result;
+        };
+
+        if (packet.length != 15 || checksum() != 0) {
+            return false;
+        }
+
+        // Bit 0-1: x-axis (10-left, 01-right, 11-middle)
+        // Bit 2-3: y-axis (01-up, 10-down, 11-middle)
+        // Bit 4-13: 10 buttons
+        // Bit 14: checksum
+        for (auto i = 0u; i < 10; i++) {                
+            state.buttons |= (~packet.bits[i + 4] & 1) << i;
+        }
+        state.axis[0] = 1 + packet.bits[0] - packet.bits[1];
+        state.axis[1] = 1 + packet.bits[3] - packet.bits[2];            
+
+        return true;
+    }    
+};
+
+template <>
+class Sidewinder::Decoder<Sidewinder::Model::SW_3DPRO> {
+public:
+    static bool decode(const Sidewinder::Packet& packet, Sidewinder::State& state) {
+        return false;
+    }    
+};
+
+inline bool Sidewinder::decode(const Packet& packet, State& state) const {
+    switch(m_model) {
+        case Model::SW_GAMEPAD:
+           return Decoder<Model::SW_GAMEPAD>::decode(packet, state);
+        case Model::SW_3DPRO:
+           return Decoder<Model::SW_3DPRO>::decode(packet, state);
+        case Model::SW_UNKNOWN:
+           break;
+    }
+    return false;
+}
+
--- firmware/gameport-adapter/SidewinderGPPro.h
+++ firmware/gameport-adapter/SidewinderGPPro.h
@@ -8,46 +8,18 @@
 
 class SidewinderGPPro : public Driver {
 public:
-   using HidDeviceType = HidDevice<SidewinderGPPro>;
 
+using HidDeviceType = HidDevice<SidewinderGPPro>;
    void init() override {
        HidDeviceType::activate();
        m_sw.reset();
    }
 
    void update() override {
-       const auto bits = m_sw.readBits();
-       if (m_sw.validate(bits) && bits.count == 15) {
-           // Input data (MSB, low active):
-           // Bit 0: checksum
-           // Bit 1-11: 10 buttons
-           // Bit 12-13: y-axis (01-up, 10-down, 11-middle)
-           // Bit 14-15: x-axis (10-left, 01-right, 11-middle)
-           //
-           // Output data (LSB, high active):
-           // Bit 0-1: x-axis (00-left, 01-middle, 10-right)
-           // Bit 2-3: y-axis (00-up, 01-middle, 10-right)
-           // Bit 4-14: 10 buttons
-           const auto decode = [](uint16_t buttons) {
-               switch(buttons) {
-                   case 0b0101: return 0b0010; // down-left
-                   case 0b0110: return 0b0000; // up-left,
-                   case 0b0111: return 0b0001; // left
-                   case 0b1001: return 0b1010; // down-right
-                   case 0b1010: return 0b0100; // up-right
-                   case 0b1011: return 0b1001; // right
-                   case 0b1101: return 0b0110; // down
-                   case 0b1110: return 0b0100; // up
-                   default:     return 0b0101; // middle
-               }              
-           };
-
-           uint16_t data = decode((bits.data >> 11) & 0b1111);
-           for (auto i = 1; i < 11; i++) {
-               data |= (~(bits.data >> i) & 1) << (14 - i);
-           }
-           HidDeviceType::send(&data, sizeof(data));
-       }
+       const auto state = m_sw.readState();
+       const uint16_t data = state.buttons << 4 | state.axis[0] << 2 | state.axis[1];
+       HidDeviceType::send(&data, sizeof(data));
+       m_sw.cooldown();
    }
 
 private:
